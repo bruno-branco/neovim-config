49de2f323e86f28633440474acb3e13bb8bea39e
Bruno Branco <brunocbranco16@gmail.com>
2025-08-29 20:06:50 -0300

    manage env files

--- lua/brunobranco/plugins/harpoon.lua
+++ lua/brunobranco/plugins/harpoon.lua
@@ -14,14 +14,38 @@ return {
 		vim.keymap.set("n", "<leader>ot", function()
 			vim.cmd("vsplit | terminal")
 			harpoon:list():replace_at(9) --for list id 9
+			vim.cmd("startinsert")
 		end)
 
-		vim.api.nvim_create_user_command("KillTerm", function()
+		vim.api.nvim_create_user_command("KT", function()
 			for _, buf in ipairs(vim.api.nvim_list_bufs()) do
 				local name = vim.api.nvim_buf_get_name(buf)
 				if name:match("^term://") then
 					vim.api.nvim_buf_delete(buf, { force = true })
 				end
+			end
+		end, {})
+
+		vim.api.nvim_create_user_command("KB", function()
+			for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+				vim.api.nvim_buf_delete(buf, { force = true })
+			end
+		end, {})
+
+		vim.keymap.set("n", "<leader>oe", function()
+			local cwd = vim.fn.getcwd()
+			local env = cwd .. "/.env"
+			local envlocal = cwd .. "/.env.local"
+
+			local target
+			if vim.fn.filereadable(env) == 1 then
+				vim.cmd("vnew .env")
+			elseif vim.fn.filereadable(envlocal) == 1 then
+				vim.cmd("vnew .env.local")
+			else
+				vim.cmd("!touch .env")
+				vim.notify("Env file does not exist, press enter to create one")
+				vim.cmd("vnew .env")
 			end
 		end, {})
 
@@ -85,10 +109,12 @@ return {
 				-- Update Harpoon slot 9 with the new buffer name
 				local new_bufname = vim.api.nvim_buf_get_name(0)
 				harpoon:list():replace_at(9, { value = new_bufname, context = {} })
+				vim.cmd("startinsert")
 			else
 				-- Buffer exists, open it in a vsplit
 				vim.cmd("vsplit")
 				vim.api.nvim_set_current_buf(bufnr)
+				vim.cmd("startinsert")
 			end
 		end)
 
